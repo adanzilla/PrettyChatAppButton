5c6dc0dbd144e59f4140a08873a90d2aa46762a1
Adan Serrano <adan.serrano@gmail.com>
2023-02-26 13:10:58 -0600

    Se agrega la base del repositorio

---
+++ whatsapp-button.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:     Whatsapp Button
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          adanzilla
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     whatsapp-button
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Whatsapp_Button
+ */
+
+// Your code starts here.
+
